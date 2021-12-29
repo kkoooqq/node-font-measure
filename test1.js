e7c013f86e8e2703048d61f57aa4c5a80a0c8113
kkoooqq <>
2021-12-28 19:05:30 -0500

    update test js

--- test1.js
+++ test1.js
@@ -5,8 +5,23 @@ const m = require('./')
     const fonts = await fontList.getFonts({disableQuoting: true})
     fonts.push('sans-serif', 'serif', 'monospace')
 
+    const fontAndWidth = {}
     for (const font of fonts) {
         let res = m(font, {fontSize: 64})
-        console.log(font, res.text)
+        fontAndWidth[font] = res.text.width
     }
+
+    const existsFontNames = []
+    for (const [fontName, width] of Object.entries(fontAndWidth)) {
+        if (
+            width !== fontAndWidth['sans-serif'].width
+            && width !== fontAndWidth['serif'].width
+            && width !== fontAndWidth['monospace'].width
+        ) {
+            console.log(fontName, width)
+            existsFontNames.push(fontName)
+        }
+    }
+
+    console.log(existsFontNames)
 })()
